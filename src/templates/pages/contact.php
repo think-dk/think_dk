412120d7f7624b82a7607dd110e99c28846e4d3d
kaestel <martin@think.dk>
2016-03-02 22:47:39 +0100

    Updated to new version

--- src/templates/pages/contact.php
+++ src/templates/pages/contact.php
@@ -68,60 +68,20 @@ $this->sharingMetaData($page);
 
 
 
-
-
 <? if($items): ?>
-	<h2>Employees, staff and consultants</h2>
+	<h2>Team</h2>
 	<ul class="items people">
 		<? foreach($items as $item): 
 			$media = $IC->sliceMedia($item); ?>
-		<li class="item person id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/NewsArticle">
+		<li class="item person vcard id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Person">
 
-			<ul class="tags">
-			<? if($item["tags"]):
-				$editing_tag = arrayKeyValue($item["tags"], "context", "editing"); ?>
-				<? if($editing_tag !== false): ?>
-				<li class="editing" title="This post is work in progress"><?= $item["tags"][$editing_tag]["value"] == "true" ? "Still editing" : $item["tags"][$editing_tag]["value"] ?></li>
-				<? endif; ?>
-				<li><a href="/posts">Posts</a></li>
-				<? foreach($item["tags"] as $item_tag): ?>
-					<? if($item_tag["context"] == $itemtype): ?>
-				<li itemprop="articleSection"><a href="/posts/tag/<?= urlencode($item_tag["value"]) ?>"><?= $item_tag["value"] ?></a></li>
-					<? endif; ?>
-				<? endforeach; ?>
-			<? endif; ?>
+			<h3 itemprop="name" class="fn name"><?= $item["name"] ?></h3>
+			<ul class="info">
+				<li itemprop="affiliation" class="affiliation">think.dk</li>
+				<li itemprop="jobTitle" class="title"><?= $item["job_title"] ?></li>
+				<li itemprop="telephone" class="tel"><?= $item["tel"] ?></li>
+				<li><a href="mailto:<?= $item["email"] ?>" itemprop="email" class="email"><?= $item["email"] ?></a></li>
 			</ul>
-
-			<h3 itemprop="headline"><?= $item["name"] ?></h3>
-
-			<!--ul class="info">
-				<li class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", strtotime($item["published_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($item["published_at"])) ?></li>
-				<li class="modified_at" itemprop="dateModified" content="<?= date("Y-m-d", strtotime($item["modified_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($item["published_at"])) ?></li>
-				<li class="author" itemprop="author"><?= $item["user_nickname"] ?></li>
-				<li class="main_entity" itemprop="mainEntityOfPage"><?= SITE_URL."/posts/".$item["sindex"] ?></li>
-				<li class="publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-					<ul class="publisher_info">
-						<li class="name" itemprop="name">think.dk</li>
-						<li class="logo" itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-							<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
-							<span class="image_width" itemprop="width" content="720"></span>
-							<span class="image_height" itemprop="height" content="405"></span>
-						</li>
-					</ul>
-				</li>
-				<li class="image_info" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-				<? if($media): ?>
-					<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/images/<?= $item["item_id"] ?>/<?= $media["variant"] ?>/720x.<?= $media["format"] ?>"></span>
-					<span class="image_width" itemprop="width" content="720"></span>
-					<span class="image_height" itemprop="height" content="<?= floor(720 / ($media["width"] / $media["height"])) ?>"></span>
-				<? else: ?>
-					<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
-					<span class="image_width" itemprop="width" content="720"></span>
-					<span class="image_height" itemprop="height" content="405"></span>
-				<? endif; ?>
-				</li>
-			</ul-->
-
 			<? if($item["description"]): ?>
 			<div class="description" itemprop="description">
 				<p><?= nl2br($item["description"]) ?></p>
@@ -132,5 +92,54 @@ $this->sharingMetaData($page);
 		<? endforeach; ?>
 	</ul>
 <? endif; ?>
+
+
+	<div itemtype="http://schema.org/Organization" itemscope class="vcard company">
+		<h2 class="name fn org" itemprop="name">think.dk</h2>
+
+		<dl class="info">
+			<dt class="address">Address</dt>
+			<dd class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+				<ul>
+					<li itemprop="streetAddress">Æbeløgade 4</li>
+					<li><span class="postal" itemprop="postalCode">2100</span> <span class="locality" itemprop="addressLocality">København Ø</span></li>
+					<li class="country" itemprop="addressCountry">Denmark</li>
+				</ul>
+			</dd>
+			<dt class="cvr">CVR</dt>
+			<dd class="cvr" itemprop="taxID">33 06 23 38</dd>
+		</dl>
+		<dl class="info">
+			<dt class="contact">Contact</dt>
+			<dd class="contact">
+				<ul>
+					<li class="email"><a href="mailto:start@think.dk" itemprop="email">start@think.dk</a></li>
+				</ul>
+			</dd>
+
+			<dt class="social">Social media</dt>
+			<dd class="social">
+				<ul>
+					<li class="facebook"><a href="https://facebook.com/think.dk">Facebook</a></li>
+					<li class="linkedin"><a href="https://www.linkedin.com/company/think-dk">LinkedIn</a></li>
+				</ul>
+			</dd>
+
+		</dl>
+
+		<dl class="info">
+			<dt class="bank">Bank</dt>
+			<dd class="bank"><a href="http://faelleskassen.dk">Fælleskassen</a></dd>
+
+			<dt class="account">Account no</dt>
+			<dd class="account">84114145172</dd>
+		</dl>
+
+	</div>
+
+
+
+
+
 
 </div>
