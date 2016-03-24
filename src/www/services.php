d5c27488b81f4fe282d5986520f8d7af4a59081f
kaestel <martin@think.dk>
2016-03-24 20:40:46 +0100

    Services first draft

---
+++ src/www/services.php
@@ -0,0 +1,34 @@
+<?php
+$access_item = false;
+if(isset($read_access) && $read_access) {
+	return;
+}
+
+include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
+
+
+$action = $page->actions();
+$IC = new Items();
+
+
+$page->bodyClass("services");
+$page->pageTitle("Services");
+
+
+// news list for tags
+// /posts/#sindex#
+if(count($action) == 1) {
+
+	$page->page(array(
+		"templates" => "services/view.php"
+	));
+	exit();
+
+}
+
+$page->page(array(
+	"templates" => "services/index.php"
+));
+exit();
+
+?>
