d5570b79c01619668c811c110c9e7c9f5e15534a
kaestel <martin@think.dk>
2016-06-07 22:02:15 +0200

    Updated share path

--- src/templates/events/index.php
+++ src/templates/events/index.php
@@ -42,7 +42,7 @@ $items = $IC->getItems(array("itemtype" => "event", "status" => 1, "where" => "e
 			<li class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", strtotime($page_item["published_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($page_item["published_at"])) ?></li>
 			<li class="modified_at" itemprop="dateModified" content="<?= date("Y-m-d", strtotime($page_item["modified_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($page_item["published_at"])) ?></li>
 			<li class="author" itemprop="author"><?= $page_item["user_nickname"] ?></li>
-			<li class="main_entity share" itemprop="mainEntityOfPage"><?= SITE_URL."/services" ?></li>
+			<li class="main_entity share" itemprop="mainEntityOfPage"><?= SITE_URL."/events" ?></li>
 			<li class="publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 				<ul class="publisher_info">
 					<li class="name" itemprop="name">think.dk</li>
