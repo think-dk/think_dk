2b94cb6a2dc5008e726f77d0da02a6e72b8667c2
kaestel <martin@think.dk>
2016-06-24 12:24:56 +0200

    membership controller updated

--- src/www/memberships.php
+++ src/www/memberships.php
@@ -8,18 +8,58 @@ include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
 
 $action = $page->actions();
-$IC = new Items();
-$itemtype = "subscriptions";
 $model = new User();
+
+// $IC = new Items();
+// $itemtype = "subscriptions";
+// $model = new User();
 
 
 $page->bodyClass("membership");
 $page->pageTitle("Memberships");
 
+if(is_array($action) && count($action)) {
 
+	// /signup/receipt
+	if($action[0] == "receipt") {
+
+		$page->page(array(
+			"templates" => "signup/receipt.php"
+		));
+		exit();
+	}
+
+	// /signup/save
+	else if($action[0] == "save" && $page->validateCsrfToken()) {
+
+		// create new user
+		$user = $model->newUser(array("newUser"));
+
+		// successful creation
+		if(isset($user["user_id"])) {
+
+			// redirect to leave POST state
+			header("Location: receipt");
+			exit();
+
+		}
+
+		// user exists
+		else if(isset($user["status"]) && $user["status"] == "USER_EXISTS") {
+			message()->addMessage("Sorry, the computer says you either have a bad memory or a bad conscience!", array("type" => "error"));
+		}
+		// something went wrong
+		else {
+			message()->addMessage("Sorry, computer says no!", array("type" => "error"));
+		}
+
+	}
+
+}
+
+// plain signup directly
+// /signup
 $page->page(array(
 	"templates" => "pages/memberships.php"
 ));
-exit();
-
 ?>
