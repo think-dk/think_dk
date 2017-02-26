6deb0e610dc14deb4ff860388e15f95a4c76a052
Martin Kæstel Nielsen <martin@think.dk>
2017-02-26 18:52:32 +0100

    Updated error page text

--- src/templates/pages/404.php
+++ src/templates/pages/404.php
@@ -5,7 +5,8 @@
 	<h2>Technology needs humanity.</h2>
 
 	<p>
-		Please send a taunting email to our developers at 
+		We're eager to solve problems.
+		Please let us know at 
 		<a href="mailto:support@think.dk?subject=Missing%20page:<?= $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>">support@think.dk</a>.
 	</p>
 </div>
