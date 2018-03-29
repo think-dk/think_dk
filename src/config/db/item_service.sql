5c5039f9f34d2556558a29c526e732ac8ed3ec41
Martin Kæstel Nielsen <martin@think.dk>
2018-03-29 02:34:46 +0200

    Update to new DB auto update syntax

--- src/config/db/item_service.sql
+++ src/config/db/item_service.sql
@@ -3,9 +3,9 @@ CREATE TABLE `SITE_DB`.`item_service` (
   `item_id` int(11) NOT NULL,
 
   `name` varchar(100) NOT NULL,
-  `classname` varchar(50) NOT NULL,
-  `description` text NOT NULL,
-  `html` text NOT NULL,
+  `classname` varchar(50) NOT NULL DEFAULT '',
+  `description` text NOT NULL DEFAULT '',
+  `html` text NOT NULL DEFAULT '',
 
   `position` int(11) DEFAULT '0',
 
