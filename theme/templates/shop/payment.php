7eafbd5b752e3659d1fc42f815347675b9fc4eb4
Martin Kæstel Nielsen <martin@think.dk>
2018-07-23 17:36:46 +0200

    login_forward update + login in payment

--- theme/templates/shop/payment.php
+++ theme/templates/shop/payment.php
@@ -8,7 +8,7 @@ $user_id = session()->value("user_id");
 $order_no = $action[1];
 $amount = "";
 
-
+// Will only return orders from current user, so no need to check order ownership
 $order = $model->getOrders(array("order_no" => $order_no));
 $membership = $UC->getMembership();
 
@@ -58,27 +58,12 @@ $payment_methods = $this->paymentMethods();
 
 	<ul class="payment_methods">
 
-		<? foreach($payment_methods as $payment_method): ?>
+	<? foreach($payment_methods as $payment_method): ?>
+		<? if($payment_method["classname"] != "disabled"): ?>
+
 		<li class="payment_method<?= $payment_method["classname"] ? " ".$payment_method["classname"] : "" ?>">
 
 			<ul class="actions">
-			<? if($payment_method["classname"] == "disabled"): ?>
-
-				<li><a class="button disabled"><?= $payment_method["name"] ?></a></li>
-
-				<? //elseif($payment_method["gateway"] && $model->canBeCharged(["user_id" => $order["user_id"], "gateway" => $payment_method["gateway"]])): ?>
-
-				<? //= $JML->oneButtonForm($payment_method["name"], "/shop/selectPaymentMethod", array(
-					// "inputs" => array("order_id" => $order["id"], "payment_method" => $payment_method["id"]),
-					// "confirm-value" => false,
-					// "static" => true,
-					// "class" => "primary",
-					// "name" => "continue",
-					// "wrapper" => "li.continue.".$payment_method["classname"],
-				// )) ?>
-
-			<? else: ?>
-
 				<?= $JML->oneButtonForm($payment_method["name"], "/shop/selectPaymentMethod", array(
 					"inputs" => array("order_id" => $order["id"], "payment_method" => $payment_method["id"]),
 					"confirm-value" => false,
@@ -87,22 +72,53 @@ $payment_methods = $this->paymentMethods();
 					"name" => "continue",
 					"wrapper" => "li.continue.".$payment_method["classname"],
 				)) ?>
-
-			<? endif; ?>
 			</ul>
 			<p><?= $payment_method["description"] ?></p>
 
 		</li>
-		<? endforeach; ?>
+		<? endif; ?>
+	<? endforeach; ?>
 
 	</ul>
 
 	<p class="note">* We pay the transaction fee – so feel free to choose a suited option with the smallest fee for us.</p>
 
-<? else: ?>
+<? else:
+
+	$model = new User();
+	$username = stringOr(getPost("username"));
+	?>
 
 	<h2>Looking to make a payment?</h2>
-	<p>You should <a href="/login?forward_url=<?= $this->url ?>">log in</a> to your account and initiate your payment from there.</p>
+	<p>Please log in to your account.</p>
+
+
+	<?= $model->formStart("?login=true", array("class" => "labelstyle:inject")) ?>
+		<?= $model->input("login_forward", ["type" => "hidden", "value" => $this->url]); ?>
+
+		<? if(message()->hasMessages()): ?>
+		<div class="messages">
+		<?
+		$all_messages = message()->getMessages();
+		message()->resetMessages();
+		foreach($all_messages as $type => $messages):
+			foreach($messages as $message): ?>
+			<p class="<?= $type ?>"><?= $message ?></p>
+			<? endforeach;?>
+		<? endforeach;?>
+		</div>
+		<? endif; ?>
+
+		<fieldset>
+			<?= $model->input("username", array("required" => true, "value" => $username)); ?>
+			<?= $model->input("password", array("required" => true)); ?>
+		</fieldset>
+
+		<ul class="actions">
+			<?= $model->submit("Log in", array("class" => "primary", "wrapper" => "li.login")) ?>
+		</ul>
+	<?= $model->formEnd() ?>
+	
 
 <? endif;?>
 
