61bb5b41eebfc707bad6cb0e392deeb106adff2c
threepwoodd <scope.dk@gmail.com>
2018-12-04 17:17:50 +0100

    Added missing settings include

--- theme/www/js/lib/seg_desktop_light_include.js
+++ theme/www/js/lib/seg_desktop_light_include.js
@@ -1,8 +1,11 @@
 // Skin
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/u-settings.js"></script>');
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-form-desktop_light.js"></script>');
 
 // Project
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
+
 document.write('<script type="text/javascript" src="/js/lib/desktop_light/i-cart.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop_light/i-memberships.js"></script>');
--- theme/www/js/lib/seg_mobile_include.js
+++ theme/www/js/lib/seg_mobile_include.js
@@ -1,1 +1,7 @@
+// Skin
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_mobile.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/u-settings.js"></script>');
+
+// Project
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
+
