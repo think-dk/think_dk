3dad2b72da864078be04941874d61a4e7b9995e5
threepwoodd <scope.dk@gmail.com>
2019-04-12 09:40:08 +0200

    Squashed 'theme/www/assets/parentnode-skin-default/' changes from d7159384..b6fb1bef
    b6fb1bef Build
    88c5e323 Update pagination
    
    git-subtree-dir: theme/www/assets/parentnode-skin-default
    git-subtree-split: b6fb1bef673db86bdb0312396241447b9182d3f5

--- js/seg_desktop.js
+++ js/seg_desktop.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2019-04-10 12:53:20
+asset-builder @ 2019-04-12 09:34:38
 */
 
 /*seg_desktop_include.js*/
