c33590b246b221c83b72fe6f73e67b606ee67162
Martin Kæstel Nielsen <martin@think.dk>
2019-10-13 19:36:55 +0200

    Update js for new form

--- theme/www/js/lib/smartphone/i-cart.js
+++ theme/www/js/lib/smartphone/i-cart.js
@@ -59,7 +59,7 @@ Util.Objects["cart"] = new function() {
 					u.f.init(quantity_form);
 
 
-					quantity_form.fields["quantity"].updated = function() {
+					quantity_form.inputs["quantity"].updated = function() {
 						u.ac(this._form.actions["update"], "primary");
 
 						this._form.submit();
@@ -90,7 +90,7 @@ Util.Objects["cart"] = new function() {
 							}
 						}
 
-						u.request(this, this.action, {"method":"post", "params":u.f.getParams(this)});
+						u.request(this, this.action, {"method":"post", "data":this.getData()});
 					}
 				}
 
