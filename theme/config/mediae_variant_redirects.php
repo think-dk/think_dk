edccb2530d20c536b62393d77f34da914fd93b4a
Martin Kæstel Nielsen <martin@think.dk>
2019-10-28 18:54:43 +0100

    Add mediae redirects

---
+++ theme/config/mediae_variant_redirects.php
@@ -0,0 +1,24 @@
+<?
+$mediae_redirects = [
+	"1197" => [
+		"HTML-6bt3eb29" => "HTMLEDITOR-html-a1f07dpg",
+		"HTML-4us6rkzn" => "HTMLEDITOR-html-rne6ec2y",
+		"HTML-pb9yozk7" => "HTMLEDITOR-html-gni1g5a5",
+		"HTML-zxbs059n" => "HTMLEDITOR-html-dc5m0d05"
+	],
+	"1208" => [
+		"HTML-4y9u72lz" => "HTMLEDITOR-html-xqsmr319",
+		"HTML-drlthqoa" => "HTMLEDITOR-html-mae8rtzf",
+		"HTML-jjg317h6" => "HTMLEDITOR-html-cl7jm7t3",
+		"HTML-qa3jbyq7" => "HTMLEDITOR-html-sazp6d4g",
+		"HTML-ttixmzgg" => "HTMLEDITOR-html-lqqnxu9l",
+		"HTML-ohlimvya" => "HTMLEDITOR-html-ued1kwie",
+		"HTML-k81f62fj" => "HTMLEDITOR-html-rif8lbkr",
+		"HTML-7mb2y89l" => "HTMLEDITOR-html-jgyiq0no"
+	],
+	"1217" => [
+		"HTML-yrahjoy8" => "HTMLEDITOR-html-93qs6lzd"
+	]
+];
+
+?>
