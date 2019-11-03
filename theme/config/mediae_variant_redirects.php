466953088d52eed3195df3ec0be2d896972be7de
Martin Kæstel Nielsen <martin@think.dk>
2019-11-03 12:32:57 +0100

    Add variant redirects

--- theme/config/mediae_variant_redirects.php
+++ theme/config/mediae_variant_redirects.php
@@ -18,6 +18,37 @@ $mediae_redirects = [
 	],
 	"1217" => [
 		"HTML-yrahjoy8" => "HTMLEDITOR-html-93qs6lzd"
+	],
+	"1047" => [
+		"HTML-5f7yqell" => "HTMLEDITOR-html-sm8y0myb",
+		"HTML-viikzcpt" => "HTMLEDITOR-html-b2s7r2ke",
+		"HTML-7590ld78" => "HTMLEDITOR-html-ok19d8sk",
+		"ppprg6e0" => "mediae-eqhnczr0"
+	],
+	"914" => [
+		"HTML-lq17tppx" => "HTMLEDITOR-html-zwgkrlqs",
+		"HTML-29pgx4xi" => "HTMLEDITOR-html-u8lcyoc4",
+		"HTML-ga2b4p80" => "HTMLEDITOR-html-qwz4zasx",
+		"kht9n67q" => "mediae-zpwximmt"
+	],
+	"1108" => [
+		"HTML-bjqkabfg" => "HTMLEDITOR-html-f68xevp9",
+		"HTML-beb8vbkk" => "HTMLEDITOR-html-67332lql",
+		"HTML-03cybmem" => "HTMLEDITOR-html-h5a8s2t6"
+	],
+	"1105" => [
+		"HTML-88gsj6x3" => "HTMLEDITOR-html-apkszll7",
+		"HTML-ojgejh4m" => "HTMLEDITOR-html-f85x9vzo",
+		"HTML-xm7b25qp" => "HTMLEDITOR-html-q1ak6v5o"
+	],
+	"1172" => [
+		"HTML-w6ov3ds7" => "HTMLEDITOR-html-bg10b5lk",
+		"HTML-r2myo5q5" => "HTMLEDITOR-html-pvz2sap6",
+		"HTML-x9jl37rr" => "HTMLEDITOR-html-hxt91kv9",
+		"HTML-qykkd9x6" => "HTMLEDITOR-html-nuuecev6"
+	],
+	"1211" => [
+		"dqnqam8k" => "mediae-ne6ofpjh"
 	]
 ];
 
