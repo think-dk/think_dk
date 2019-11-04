73f10abf5b187d61f52f769d7da80f9f130260b9
Martin Kæstel Nielsen <martin@think.dk>
2019-11-04 10:47:33 +0100

    Add mediae redirects

--- theme/config/mediae_variant_redirects.php
+++ theme/config/mediae_variant_redirects.php
@@ -1,5 +1,63 @@
 <?
 $mediae_redirects = [
+	"914" => [
+		"HTML-lq17tppx" => "HTMLEDITOR-html-zwgkrlqs",
+		"HTML-29pgx4xi" => "HTMLEDITOR-html-u8lcyoc4",
+		"HTML-ga2b4p80" => "HTMLEDITOR-html-qwz4zasx",
+		"kht9n67q" => "mediae-zpwximmt"
+	],
+	"1021" => [
+		"HTML-tv0y3dub" => "HTMLEDITOR-html-h2ft53zl",
+		"HTML-rg4i791q" => "HTMLEDITOR-html-4ovx6pwb",
+		"HTML-wf1k0tzf" => "HTMLEDITOR-html-kk6n5n4s",
+		"HTML-3dpnzaik" => "HTMLEDITOR-html-ucwdekaj",
+		"HTML-tdddohpe" => "HTMLEDITOR-html-nytwclsv",
+		"HTML-4veg4cle" => "HTMLEDITOR-html-6sjcwksk"
+	],
+	"1034" => [
+		"HTML-3g7a3t6u" => "HTMLEDITOR-html-dp5k0unh"
+	],
+	"1047" => [
+		"HTML-5f7yqell" => "HTMLEDITOR-html-sm8y0myb",
+		"HTML-viikzcpt" => "HTMLEDITOR-html-b2s7r2ke",
+		"HTML-7590ld78" => "HTMLEDITOR-html-ok19d8sk",
+		"ppprg6e0" => "mediae-eqhnczr0"
+	],
+	"1052" => [
+		"HTML-15wui87g" => "HTMLEDITOR-html-lm4om3pt"
+	],
+	"1100" => [
+		"HTML-62ouvxpt" => "HTMLEDITOR-html-p6udmph9",
+		"HTML-14gn5pr7" => "HTMLEDITOR-html-8skk3h83",
+		"HTML-h505bgff" => "HTMLEDITOR-html-lwnjq3mo"
+	],
+	"1105" => [
+		"HTML-88gsj6x3" => "HTMLEDITOR-html-apkszll7",
+		"HTML-ojgejh4m" => "HTMLEDITOR-html-f85x9vzo",
+		"HTML-xm7b25qp" => "HTMLEDITOR-html-q1ak6v5o"
+	],
+	"1108" => [
+		"HTML-bjqkabfg" => "HTMLEDITOR-html-f68xevp9",
+		"HTML-beb8vbkk" => "HTMLEDITOR-html-67332lql",
+		"HTML-03cybmem" => "HTMLEDITOR-html-h5a8s2t6"
+	],
+	"1172" => [
+		"HTML-w6ov3ds7" => "HTMLEDITOR-html-bg10b5lk",
+		"HTML-r2myo5q5" => "HTMLEDITOR-html-pvz2sap6",
+		"HTML-x9jl37rr" => "HTMLEDITOR-html-hxt91kv9",
+		"HTML-qykkd9x6" => "HTMLEDITOR-html-nuuecev6"
+	],
+	"1186" => [
+		"HTML-wnjep0jj" => "HTMLEDITOR-html-cqw6nggf",
+		"HTML-ag9z566c" => "HTMLEDITOR-html-vzh6c7sy",
+		"HTML-txgna2ie" => "HTMLEDITOR-html-smnjrf94",
+		"HTML-gmrs6fxc" => "HTMLEDITOR-html-zt9eua9v",
+		"HTML-peoy0i0v" => "HTMLEDITOR-html-ew1kzbdw",
+		"HTML-08hphlja" => "HTMLEDITOR-html-43xv63jx",
+		"HTML-focdbzkc" => "HTMLEDITOR-html-xs6ovrh0",
+		"HTML-33me127g" => "HTMLEDITOR-html-zlu4ls6p",
+		"HTML-u68omhmz" => "HTMLEDITOR-html-0b0njoh8"
+	],
 	"1197" => [
 		"HTML-6bt3eb29" => "HTMLEDITOR-html-a1f07dpg",
 		"HTML-4us6rkzn" => "HTMLEDITOR-html-rne6ec2y",
@@ -16,61 +74,11 @@ $mediae_redirects = [
 		"HTML-k81f62fj" => "HTMLEDITOR-html-rif8lbkr",
 		"HTML-7mb2y89l" => "HTMLEDITOR-html-jgyiq0no"
 	],
-	"1217" => [
-		"HTML-yrahjoy8" => "HTMLEDITOR-html-93qs6lzd"
-	],
-	"1047" => [
-		"HTML-5f7yqell" => "HTMLEDITOR-html-sm8y0myb",
-		"HTML-viikzcpt" => "HTMLEDITOR-html-b2s7r2ke",
-		"HTML-7590ld78" => "HTMLEDITOR-html-ok19d8sk",
-		"ppprg6e0" => "mediae-eqhnczr0"
-	],
-	"914" => [
-		"HTML-lq17tppx" => "HTMLEDITOR-html-zwgkrlqs",
-		"HTML-29pgx4xi" => "HTMLEDITOR-html-u8lcyoc4",
-		"HTML-ga2b4p80" => "HTMLEDITOR-html-qwz4zasx",
-		"kht9n67q" => "mediae-zpwximmt"
-	],
-	"1108" => [
-		"HTML-bjqkabfg" => "HTMLEDITOR-html-f68xevp9",
-		"HTML-beb8vbkk" => "HTMLEDITOR-html-67332lql",
-		"HTML-03cybmem" => "HTMLEDITOR-html-h5a8s2t6"
-	],
-	"1105" => [
-		"HTML-88gsj6x3" => "HTMLEDITOR-html-apkszll7",
-		"HTML-ojgejh4m" => "HTMLEDITOR-html-f85x9vzo",
-		"HTML-xm7b25qp" => "HTMLEDITOR-html-q1ak6v5o"
-	],
-	"1172" => [
-		"HTML-w6ov3ds7" => "HTMLEDITOR-html-bg10b5lk",
-		"HTML-r2myo5q5" => "HTMLEDITOR-html-pvz2sap6",
-		"HTML-x9jl37rr" => "HTMLEDITOR-html-hxt91kv9",
-		"HTML-qykkd9x6" => "HTMLEDITOR-html-nuuecev6"
-	],
 	"1211" => [
 		"dqnqam8k" => "mediae-ne6ofpjh"
 	],
-	"1186" => [
-		"HTML-wnjep0jj" => "HTMLEDITOR-html-cqw6nggf",
-		"HTML-ag9z566c" => "HTMLEDITOR-html-vzh6c7sy",
-		"HTML-txgna2ie" => "HTMLEDITOR-html-smnjrf94",
-		"HTML-gmrs6fxc" => "HTMLEDITOR-html-zt9eua9v",
-		"HTML-peoy0i0v" => "HTMLEDITOR-html-ew1kzbdw",
-		"HTML-08hphlja" => "HTMLEDITOR-html-43xv63jx",
-		"HTML-focdbzkc" => "HTMLEDITOR-html-xs6ovrh0",
-		"HTML-33me127g" => "HTMLEDITOR-html-zlu4ls6p",
-		"HTML-u68omhmz" => "HTMLEDITOR-html-0b0njoh8"
-	],
-	"1100" => [
-		"HTML-62ouvxpt" => "HTMLEDITOR-html-p6udmph9",
-		"HTML-14gn5pr7" => "HTMLEDITOR-html-8skk3h83",
-		"HTML-h505bgff" => "HTMLEDITOR-html-lwnjq3mo"
-	],
-	"1052" => [
-		"HTML-15wui87g" => "HTMLEDITOR-html-lm4om3pt"
-	],
-	"1034" => [
-		"HTML-3g7a3t6u" => "HTMLEDITOR-html-dp5k0unh"
+	"1217" => [
+		"HTML-yrahjoy8" => "HTMLEDITOR-html-93qs6lzd"
 	]
 ];
 
