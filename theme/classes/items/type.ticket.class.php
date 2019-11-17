43ea88bc50ccc1a59af174efa3a70197635e211f
Martin Kæstel Nielsen <martin@think.dk>
2019-11-17 20:44:21 +0100

    Update ticket model

--- theme/classes/items/type.ticket.class.php
+++ theme/classes/items/type.ticket.class.php
@@ -115,13 +115,12 @@ class TypeTicket extends Itemtype {
 
 		$query = new Query();
 		$sql = "SELECT SUM(quantity) as sum FROM ".SITE_DB.".shop_orders as orders, ".SITE_DB.".shop_order_items as items WHERE items.item_id = $item_id AND items.order_id = orders.id AND orders.status != 3";
-		// debug([$sql]);
+
 		$query->sql($sql);
 		$count = $query->result(0, "sum");
 
-		// debug([$count]);
-
 		return $count ? intval($count) : 0;
+
 	}
 
 	// Find specific tickets in carts
@@ -132,11 +131,10 @@ class TypeTicket extends Itemtype {
 
 		$query = new Query();
 		$sql = "SELECT items.id, items.quantity, carts.modified_at FROM ".SITE_DB.".shop_carts as carts, ".SITE_DB.".shop_cart_items as items WHERE items.item_id = $item_id AND carts.id = items.cart_id";
-		// debug([$sql, strtotime("- 15 MIN"), date("H:i", strtotime("- 15 MIN"))]);
 
 		$query->sql($sql);
 		$cart_items = $query->results();
-		// debug([$cart_items]);
+
 		foreach($cart_items as $cart_item) {
 			if(strtotime($cart_item["modified_at"]) < strtotime("- 15 MIN")) {
 
@@ -145,18 +143,16 @@ class TypeTicket extends Itemtype {
 
 			}
 			else {
+
 				$count += intval($cart_item["quantity"]);
+
 			}
 		}
-		// debug([$count]);
 
-		// print_r($carts);
 		return $count;
 	}
 
 	function ordered($order_item, $order) {
-
-		// debug([$order_item, $order]);
 
 		// check for subscription error
 		if($order && $order["user_id"] && $order_item && $order_item["item_id"]) {
@@ -164,15 +160,14 @@ class TypeTicket extends Itemtype {
 			$item_id = $order_item["item_id"];
 			$user_id = $order["user_id"];
 
+
 			global $page;
-			$page->addLog("ticket->ordered: item_id:$item_id, user_id:$user_id, order_id:".$order["id"]);
-
-			// variables for email
-			$price = formatPrice(["price" => $order_item["total_price"], "vat" => $order_item["total_vat"], "country" => $order["country"], "currency" => $order["currency"]]);
+			$page->addLog("ticket->ordered: item_id:$item_id, user_id:$user_id, order_id:".$order["id"].", order_item_id:".$order_item["id"]);
 
 
-			$ticket = $this->issueTicket($item_id, $user_id, $order["id"], $price);
-			
+			// Issue ticket(s)
+			$ticket = $this->issueTicket($item_id, $user_id, ["order" => $order, "order_item" => $order_item]);
+
 		}
 
 	}
@@ -181,67 +176,127 @@ class TypeTicket extends Itemtype {
 		
 	}
 
-	function issueTicket($item_id, $user_id, $order_id = false, $price = false) {
+	function issueTicket($item_id, $user_id, $_options = false) {
+
+		$order = false;
+		$order_item = false;
+
+		$quantity = 1;
+
+		// overwrite defaults
+		if($_options !== false) {
+			foreach($_options as $_option => $_value) {
+				switch($_option) {
+
+					case "order"           : $order            = $_value; break;
+					case "order_item"      : $order_item       = $_value; break;
+
+					case "quantity"        : $quantity         = $_value; break;
+				}
+			}
+		}
+		
 
 		$query = new Query();
 		$query->checkDbExistence($this->db_user_tickets);
 
-		$sql = "SELECT ticket_no FROM ".$this->db_user_tickets." WHERE item_id = $item_id ORDER BY id desc LIMIT 1";
-		// debug([$sql]);
-		if($query->sql($sql)) {
-			preg_match("/\-([0-9]+)$/", $query->result(0, "ticket_no"), $match);
-			$ticket_count = intval($match[1])+1;
+
+		if($user_id && $item_id) {
+
+			// Tickets based on order
+			if($order && $order_item) {
+
+				// Use quantity from order
+				$quantity = $order_item["quantity"];
+
+				// variables for email
+				$total_price = formatPrice(["price" => $order_item["total_price"], "vat" => $order_item["total_vat"], "country" => $order["country"], "currency" => $order["currency"]]);
+
+			}
+			// Free ticket
+			else {
+
+				$total_price = "0,-";
+
+			}
+
+
+			// Get item information
+			$IC = new Items();
+			$item = $IC->getItem(["id" => $item_id, "extend" => true]);
+			$message_id = $item["ordered_message_id"];
+
+			$ticket_files = [];
+
+
+			// Generate ticket
+			for($i = 0; $i < $quantity; $i++) {
+
+				// Decide on next ticket number
+				$sql = "SELECT ticket_no FROM ".$this->db_user_tickets." WHERE item_id = $item_id ORDER BY id desc LIMIT 1";
+				if($query->sql($sql)) {
+					preg_match("/\-([0-9]+)$/", $query->result(0, "ticket_no"), $match);
+					$issued_ticket_counter = intval($match[1])+1;
+				}
+				else {
+					$issued_ticket_counter = 1;
+				}
+
+				// Create ticket number
+				$ticket_no = $item_id."-".time()."-".($issued_ticket_counter);
+
+				// Insert ticket
+				$sql = "INSERT INTO ".$this->db_user_tickets." SET user_id = $user_id, item_id = $item_id, ticket_no = '$ticket_no'";
+				if($order_item) {
+					$sql .= ", order_item_id = ".$order_item["id"];
+				}
+				$query->sql($sql);
+
+				// Add batch information
+				$batch = ($quantity > 1 ? "(".($i+1)."/$quantity)" : "");
+
+				// Collect ticket files
+				$ticket_files[] = $this->generateTicket($item_id, $ticket_no, $batch);
+
+
+				global $page;
+				$page->addLog("ticket->issueTicket: item_id:$item_id, user_id:$user_id, order_item_id:".$order_item["id"].", quantity:".$quantity.", ticket_no:".$ticket_no.($batch ? ", batch:".$batch : ""));
+
+			}
+
+
+			// Send ticket email
+			$model = $IC->typeObject("message");
+			$model->sendMessage([
+				"item_id" => $message_id, 
+				"user_id" => $user_id, 
+				"values" => [
+					"QUANTITY" => $quantity,
+					"PRICE" => $total_price,
+					"EVENT_NAME" => $item["name"],
+					// "TICKET_NO" => $ticket_no,
+					"TICKET_INFORMATION" => nl2br($item["ticket_information"])
+				],
+				"attachments" => $ticket_files
+			]);
+
+			message()->resetMessages();
+
 		}
-		else {
-			$ticket_count = 1;
-		}
-
-		$ticket_no = $item_id."-".time()."-".$ticket_count;
-
-		$sql = "INSERT INTO ".$this->db_user_tickets." SET user_id = $user_id, item_id = $item_id, ticket_no = '$ticket_no'";
-		if($order_id) {
-			$sql .= ", order_id = $order_id";
-		}
-
-		$query->sql($sql);
-		$ticket_id = $query->lastInsertId();
-
-
-		$IC = new Items();
-		$item = $IC->getItem(["id" => $item_id, "extend" => true]);
-		$message_id = $item["ordered_message_id"];
-
-		$ticket_file = $this->generateTicket($ticket_no);
-
-		$model = $IC->typeObject("message");
-
-		$model->sendMessage([
-			"item_id" => $message_id, 
-			"user_id" => $user_id, 
-			"values" => [
-				"PRICE" => ($price ? $price : "0,-"),
-				"EVENT_NAME" => $item["name"],
-				"TICKET_NO" => $ticket_no,
-				"TICKET_INFORMATION" => nl2br($item["ticket_information"])
-			],
-			"attachments" => $ticket_file
-		]);
-
-
-		global $page;
-		$page->addLog("ticket->issueTicket: item_id:$item_id, user_id:$user_id, order_id:".$order_id);
 
 	}
 
 	function getTicketInfo($ticket_no) {
 
 		$query = new Query();
-		$sql = "SELECT user_id, item_id FROM ".$this->db_user_tickets." WHERE ticket_no = '$ticket_no'";
+		$sql = "SELECT user_id, item_id, order_item_id FROM ".$this->db_user_tickets." WHERE ticket_no = '$ticket_no'";
+		// debug([$sql]);
 
 		if($query->sql($sql)) {
 
 			$user_id = $query->result(0, "user_id");
 			$item_id = $query->result(0, "item_id");
+			$order_item_id = $query->result(0, "order_item_id");
 
 			$ticket_info = [];
 			$IC = new Items();
@@ -250,51 +305,55 @@ class TypeTicket extends Itemtype {
 			$UC = new User();
 			$ticket_info["user"] = $UC->getUserInfo(["user_id" => $user_id]);
 
-			return $ticket_info;
+			if($order_item_id) {
+				$SC = new Shop();
+
+				$sql = "SELECT * FROM ".$SC->db_order_items." WHERE id = '$order_item_id'";
+				if($query->sql($sql)) {
+					$order_item = $query->result(0);
+					$order = $SC->getOrders(["order_id" => $order_item["order_id"]]);
+					if($order) {
+						$ticket_info["price"] = formatPrice(["price" => $order_item["unit_price"], "vat" => $order_item["unit_vat"], "country" => $order["country"], "currency" => $order["currency"]]);
+						return $ticket_info;
+					}
+				}
+
+			}
+			else if($ticket_info["item"] && $ticket_info["name"]) {
+				return $ticket_info;
+			}
 
 		}
 
 		return false;
 	}
 
-	function generateTicket($ticket_no) {
+	function generateTicket($item_id, $ticket_no, $batch = false) {
 
-		// prepare print request url
-		$url = escapeshellarg(SITE_URL."/tickets/print/".$ticket_no);
-		// prepare save path
-		$ticket_file = PRIVATE_FILE_PATH."/ticket/".$ticket_no.".pdf";
+		if(is_string($ticket_no)) {
 
-		$fs = new FileSystem();
-		$fs->makeDirRecursively(dirname($ticket_file));
+			// prepare print request url
+			$url = SITE_URL."/tickets/print/$ticket_no".($batch ? "?batch=".urlencode($batch) : "");
+			debug([$url]);
 
-		include_once("classes/helpers/pdf.class.php");
+			// prepare save path
+			$ticket_file = PRIVATE_FILE_PATH."/$item_id/ticket/$ticket_no/pdf";
+			$public_ticket_file = PUBLIC_FILE_PATH."/$item_id/$ticket_no/$ticket_no.pdf";
 
-		$pdf = new PDF();
-		$pdf->create($url, $ticket_file, ["format" => "A5", "delay" => 5000]);
+			$fs = new FileSystem();
+			$fs->makeDirRecursively(dirname($ticket_file));
 
+			include_once("classes/helpers/pdf.class.php");
 
+			$pdf = new PDF();
+			$pdf->create($url, $ticket_file, ["format" => "A5", "delay" => 1000, "cookie" => ["name" => "PHPSESSID", "value" => $_COOKIE["PHPSESSID"]]]);
 
+			$fs = new FileSystem();
+			$fs->copy($ticket_file, $public_ticket_file);
 
-		// // prepare request url
-		// $url = escapeshellarg(SITE_URL."/tickets/print/".$ticket_no);
-		//
-		//
-		// // prepare save path + declaration id
-		// $ticket_file = PRIVATE_FILE_PATH."/ticket/".$ticket_no.".pdf";
-		//
-		// $fs = new FileSystem();
-		// $fs->makeDirRecursively(dirname($ticket_file));
-		//
-		// // Putting together the command for `shell_exec()`
-		// $command = wkhtmltoPath()." -s A6 $url --javascript-delay 4000 --no-stop-slow-scripts --enable-javascript $ticket_file";
-		// // debug([$command]);
-		//
-		// // Generate the image
-		// $output = shell_exec($command." 2>&1");
-		// // debug([$output]);
+			return $public_ticket_file;
+		}
 
-
-		return $ticket_file;
 	}
 
 }
--- theme/config/db/user_item_tickets.sql
+++ theme/config/db/user_item_tickets.sql
@@ -3,7 +3,8 @@ CREATE TABLE `SITE_DB`.`user_item_tickets` (
   `user_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
 
-  `order_id` int(11) DEFAULT NULL,
+  `order_item_id` int(11) DEFAULT NULL,
+
   `ticket_no` varchar(20) NOT NULL,
 
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -11,8 +12,8 @@ CREATE TABLE `SITE_DB`.`user_item_tickets` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `item_id` (`item_id`),
-  KEY `order_id` (`order_id`),
-  CONSTRAINT `user_item_tickets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `SITE_DB`.`users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+  KEY `order_item_id` (`order_item_id`),
+  CONSTRAINT `user_item_tickets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `SITE_DB`.`users` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `user_item_tickets_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `SITE_DB`.`items` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `user_item_tickets_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `SITE_DB`.`shop_orders` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `user_item_tickets_ibfk_3` FOREIGN KEY (`order_item_id`) REFERENCES `SITE_DB`.`shop_order_items` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
