ebd28911a5aca7bf3d3e1907d94fff5f23e234e3
Martin Kæstel Nielsen <martin@think.dk>
2020-02-26 00:36:42 +0100

    Fri os

---
+++ theme/templates/pages/friosfradevoksnes.php
@@ -0,0 +1,60 @@
+<?
+global $action;
+global $IC;
+
+$page_item = $IC->getItem(array("tags" => "page:Fri os fra de voksnes", "status" => 1, "extend" => array("comments" => true, "user" => true, "mediae" => true, "tags" => true)));
+
+if($page_item) {
+	$this->sharingMetaData($page_item);
+}
+?>
+<div class="scene frios i:scene">
+
+<? if($page_item): 
+	$media = $IC->sliceMediae($page_item, "single_media"); ?>
+	<div class="article i:article id:<?= $page_item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
+
+		<? if($media): ?>
+		<div class="image item_id:<?= $page_item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+		<? endif; ?>
+
+
+		<?= $HTML->articleTags($page_item, [
+			"context" => false
+		]) ?>
+
+
+		<h1 itemprop="headline"><?= $page_item["name"] ?></h1>
+
+		<? if($page_item["subheader"]): ?>
+		<h2 itemprop="alternativeHeadline"><?= $page_item["subheader"] ?></h2>
+		<? endif; ?>
+
+
+		<?= $HTML->articleInfo($page_item, "/the-real-black-friday", [
+			"media" => $media,
+			"sharing" => true
+		]) ?>
+
+
+		<? if($page_item["html"]): ?>
+		<div class="articlebody" itemprop="articleBody">
+			<?= $page_item["html"] ?>
+		</div>
+		<? endif; ?>
+
+
+		<?= $HTML->frontendComments($page_item, "/janitor/admin/page/addComment") ?>
+
+	</div>
+
+
+
+<? else:?>
+
+	<h1>Fri os fra de voksnes</h1>
+	<p>This page is currently being updated.</p>
+
+<? endif; ?>
+
+</div>
