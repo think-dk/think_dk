8af6d70c60da81bc1c662f321a90bfb8e18e033f
Martin Kæstel Nielsen <martin@think.dk>
2020-02-26 13:50:11 +0100

    Add payment info to invoice

--- theme/templates/janitor/shop/order/invoice-payment-info.php
+++ theme/templates/janitor/shop/order/invoice-payment-info.php
@@ -1,1 +1,14 @@
-<h3>Bank information</h3>
+<h3>Bank information:</h3>
+<dl class="info financial">
+	<dt class="bank">Bank</dt>
+	<dd class="bank">Fælleskassen</dd>
+
+	<dt class="account">Account no</dt>
+	<dd class="account">8411 4145172</dd>
+
+	<dt class="account">IBAN</dt>
+	<dd class="account">DK3184110004145172</dd>
+
+	<dt class="account">SWIFT/BIC</dt>
+	<dd class="account">FAELDKK1</dd>
+</dl>
