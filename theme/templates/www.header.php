358acee8e3224a0b5209f486d9609cdb58da4786
Martin Kæstel Nielsen <martin@think.dk>
2020-03-29 23:28:31 +0200

    Build

--- theme/templates/www.header.php
+++ theme/templates/www.header.php
@@ -28,8 +28,8 @@ if(defined("SITE_SHOP") && SITE_SHOP) {
 	<link type="text/css" rel="stylesheet" media="all" href="/css/lib/seg_<?= $this->segment() ?>_include.css" />
 	<script type="text/javascript" src="/js/lib/seg_<?= $this->segment() ?>_include.js"></script>
 <? } else { ?>
-	<link type="text/css" rel="stylesheet" media="all" href="/css/seg_<?= $this->segment() ?>.css?rev=20200329-232112" />
-	<script type="text/javascript" src="/js/seg_<?= $this->segment() ?>.js?rev=20200329-232112"></script>
+	<link type="text/css" rel="stylesheet" media="all" href="/css/seg_<?= $this->segment() ?>.css?rev=20200329-232813" />
+	<script type="text/javascript" src="/js/seg_<?= $this->segment() ?>.js?rev=20200329-232813"></script>
 <? } ?>
 
 	<?= $this->headerIncludes() ?>
