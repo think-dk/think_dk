b7914b4ae36e287bc529afbea3c175c8c0f4417b
Martin Kæstel Nielsen <martin@think.dk>
2020-07-02 21:40:56 +0200

    Update about page

--- theme/templates/pages/about.php
+++ theme/templates/pages/about.php
@@ -9,7 +9,11 @@ if($page_item) {
 }
 
 $itemtype = "person";
-$items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => $itemtype.".position", "extend" => array("tags" => true, "readstate" => true, "mediae" => true, "user" => true)));
+$daily_items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => $itemtype.".position", "tags" => "person:daily", "extend" => array("tags" => true, "readstate" => true, "mediae" => true, "user" => true)));
+$volunteer_items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => $itemtype.".position", "tags" => "person:volunteer", "extend" => array("tags" => true, "readstate" => true, "mediae" => true, "user" => true)));
+
+$board_items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => $itemtype.".position", "tags" => "person:board", "extend" => array("tags" => true, "readstate" => true, "mediae" => true, "user" => true)));
+$owner_items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => $itemtype.".position", "tags" => "person:owner", "extend" => array("tags" => true, "readstate" => true, "mediae" => true, "user" => true)));
 
 ?>
 <div class="scene about i:scene">
@@ -60,11 +64,38 @@ $items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => 
 <? endif; ?>
 
 
-<? if($items): ?>
+<? if($daily_items || $volunteer_items || $board_items || $owner_items): ?>
 	<div class="teams">
 		<h2>Behind the scenes</h2>
+
+		<? if($daily_items): ?>
+		<h3>Daily operations / Primary contacts</h3>
 		<ul class="items people">
-			<? foreach($items as $item): ?>
+			<? foreach($daily_items as $item): ?>
+			<li class="item person vcard id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Person">
+
+				<h4 itemprop="name" class="fn name"><?= $item["name"] ?></h4>
+				<ul class="info">
+					<li itemprop="affiliation" class="affiliation">think.dk</li>
+					<li itemprop="jobTitle" class="title"><?= $item["job_title"] ?></li>
+					<li itemprop="telephone" class="tel" content="<?= $item["tel"] ?>"><?= $item["tel"] ?></li>
+					<li><a href="mailto:<?= $item["email"] ?>" itemprop="email" class="email" content="<?= $item["email"] ?>"><?= $item["email"] ?></a></li>
+				</ul>
+				<? if($item["html"]): ?>
+				<div class="description" itemprop="description">
+					<?= $item["html"] ?>
+				</div>
+				<? endif; ?>
+
+			</li>
+			<? endforeach; ?>
+		</ul>
+		<? endif; ?>
+
+		<? if($volunteer_items): ?>
+		<h3>Volunteers</h3>
+		<ul class="items people">
+			<? foreach($volunteer_items as $item): ?>
 			<li class="item person vcard id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Person">
 
 				<h3 itemprop="name" class="fn name"><?= $item["name"] ?></h3>
@@ -83,6 +114,56 @@ $items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => 
 			</li>
 			<? endforeach; ?>
 		</ul>
+		<? endif; ?>
+
+		<? if($board_items): ?>
+		<h3>The Board</h3>
+		<ul class="items people">
+			<? foreach($board_items as $item): ?>
+			<li class="item person vcard id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Person">
+
+				<h3 itemprop="name" class="fn name"><?= $item["name"] ?></h3>
+				<ul class="info">
+					<li itemprop="affiliation" class="affiliation">think.dk</li>
+					<li itemprop="jobTitle" class="title"><?= $item["job_title"] ?></li>
+					<li itemprop="telephone" class="tel" content="<?= $item["tel"] ?>"><?= $item["tel"] ?></li>
+					<li><a href="mailto:<?= $item["email"] ?>" itemprop="email" class="email" content="<?= $item["email"] ?>"><?= $item["email"] ?></a></li>
+				</ul>
+				<? if($item["html"]): ?>
+				<div class="description" itemprop="description">
+					<?= $item["html"] ?>
+				</div>
+				<? endif; ?>
+
+			</li>
+			<? endforeach; ?>
+		</ul>
+		<? endif; ?>
+
+		<? if($owner_items): ?>
+		<h3>Owners</h3>
+		<ul class="items people">
+			<? foreach($owner_items as $item): ?>
+			<li class="item person vcard id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Person">
+
+				<h3 itemprop="name" class="fn name"><?= $item["name"] ?></h3>
+				<ul class="info">
+					<li itemprop="affiliation" class="affiliation">think.dk</li>
+					<li itemprop="jobTitle" class="title"><?= $item["job_title"] ?></li>
+					<li itemprop="telephone" class="tel" content="<?= $item["tel"] ?>"><?= $item["tel"] ?></li>
+					<li><a href="mailto:<?= $item["email"] ?>" itemprop="email" class="email" content="<?= $item["email"] ?>"><?= $item["email"] ?></a></li>
+				</ul>
+				<? if($item["html"]): ?>
+				<div class="description" itemprop="description">
+					<?= $item["html"] ?>
+				</div>
+				<? endif; ?>
+
+			</li>
+			<? endforeach; ?>
+		</ul>
+		<? endif; ?>
+
 	</div>
 <? endif; ?>
 
