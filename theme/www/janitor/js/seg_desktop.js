8e5eb8b8df7bd02bae28d0cebb370385278bc0e4
kaestel <martin@think.dk>
2020-09-10 07:37:28 +0200

    Build Janitor

--- theme/www/janitor/js/seg_desktop.js
+++ theme/www/janitor/js/seg_desktop.js
@@ -1,5 +1,5 @@
 /*
-asset-builder @ 2020-08-06 10:42:04
+asset-builder @ 2020-09-10 07:36:52
 */
 
 /*seg_desktop_include.js*/
@@ -9711,6 +9711,83 @@ Util.Modules["defaultComments"] = new function() {
 		var i, node;
 		for(i = 0; node = div.comments[i]; i++) {
 			div.initComment(node);
+		}
+	}
+}
+
+
+/*m-default_editors.js*/
+Util.Modules["defaultEditors"] = new function() {
+	this.init = function(div) {
+		div.item_id = u.cv(div, "item_id");
+		div.remove_editor_url = div.getAttribute("data-editor-remove");
+		div.csrf_token = div.getAttribute("data-csrf-token");
+		div._form_editors = u.qs("form.editors", div);
+		div._list_editors = u.qs("ul.editors", div);
+		if(div._form_editors) {
+			div._form_editors.div = div;
+			u.f.init(div._form_editors);
+			div._form_editors.submitted = function(iN) {
+				this.response = function(response) {
+					page.notify(response);
+					if(response.cms_status == "success" && response.cms_object && response.cms_object !== true) {
+						if(!this.div._list_editors) {
+							this.div._list_editors = u.ae(this.parentNode, "ul", {"class":"items editors"});
+							this.parentNode.insertBefore(this.div._list_editors, this);
+							var p_no_editors = u.qs("p", this.div);
+							if(p_no_editors) {
+								p_no_editors.parentNode.removeChild(p_no_editors);
+							}
+						}
+						var editor_li = u.ae(this.div._list_editors, "li", {"class":"editor editor_id:"+response.cms_object["id"]});
+						u.ae(editor_li, "h3", {"html":response.cms_object["nickname"]});
+						this.div.initEditor(editor_li);
+						this.reset();
+					}
+					else if(response.cms_status == "success" && response.cms_object && response.cms_object === true) {
+						this.reset();
+					}
+				}
+				u.request(this, this.action, {"method":"post", "data" : this.getData()});
+			}
+		}
+		div.initEditor = function(node) {
+			node.div = this;
+			if(this.remove_editor_url) {
+				node.editor_id = u.cv(node, "editor_id");
+				node._ul_actions = u.ae(node, "ul", {"class":"actions"});
+				node._li_remove = u.ae(node._ul_actions, "li", {"class":"remove"});
+				node._form_remove = u.f.addForm(node._li_remove, {
+					"action":this.remove_editor_url, 
+					"class":"remove"
+				});
+				node._form_remove.node = node;
+				u.f.addField(node._form_remove, {
+					"type":"hidden",
+					"name":"csrf-token", 
+					"value":div.csrf_token
+				});
+				u.f.addField(node._form_remove, {
+					"type":"hidden",
+					"name":"editor_id", 
+					"value":node.editor_id
+				});
+				u.f.addAction(node._form_remove, {
+					"value":"Remove",
+					"class":"button remove"
+				});
+				node._form_remove.setAttribute("data-success-function", "removed");
+				node._form_remove.setAttribute("data-confirm-value", "Are you sure?");
+				u.m.oneButtonForm.init(node._form_remove);
+				node._form_remove.removed = function(response) {
+					this.node.parentNode.removeChild(this.node);
+				}
+			}
+		}
+		div.editors = u.qsa("li.editor", div._list_editors);
+		var i, node;
+		for(i = 0; node = div.editors[i]; i++) {
+			div.initEditor(node);
 		}
 	}
 }
