a8518ea0f2f4412c8a43a2729404e11671a65587
kaestel <martin@think.dk>
2020-10-19 11:40:31 +0200

    Squashed 'theme/www/assets/parentnode-skin-default/' changes from 728c61e..02ef70c
    02ef70c build
    998f168 Update manipulator
    a528da2 build
    
    git-subtree-dir: theme/www/assets/parentnode-skin-default
    git-subtree-split: 02ef70c22dbd5b6c43d5114ee0f2b1f47a55bb8b

--- js/manipulator/merged/seg_desktop_light.js
+++ js/manipulator/merged/seg_desktop_light.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.3-parentnode-skin-default Copyright 2019 https://manipulator.parentnode.dk
-js-merged @ 2020-06-25 11:53:47
+js-merged @ 2020-10-19 11:37:08
 */
 
 /*seg_desktop_light_include.js*/
